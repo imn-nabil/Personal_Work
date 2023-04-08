5021d6c35aef98a347fd1da2553bd143aa334292
imn-nabil <imnnabil1971@gmail.com>
2023-04-08 21:04:35 +0600

    upload dawar drkr dicchi

---
+++ memoization testing.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+typedef long long int ll;
+ll term[100000000];
+ll fib(int a)
+{
+    static int prv,nxt;
+    if(a<1) {return 1;}
+    else{
+        printf("")
+        return
+    }
+}
+
+int main()
+{
+    //printf("%lld\n",fib(1000));
+
+    fib(5);
+
+    return 0;
+}
+
